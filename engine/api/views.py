a7b903e1e9160ce95fc0f8febae2dd7b2d7c5e13
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2024-03-08 16:31:10 +0000

    finally got api index loading

--- engine/api/views.py
+++ engine/api/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return JsonResponse("Hello, world. You're at the polls index.")
+    return JsonResponse({
+        "message": "We're alive! Let's change the world!!"
+    })
--- engine/engine/urls.py
+++ engine/engine/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+urlpatterns = router.urls
 
 urlpatterns = [
     path('', include('api.urls')),
