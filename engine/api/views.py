4938856c422159b72883a32a07f2bcc8cbd8737a
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2024-03-08 15:47:56 +0000

    init commit

---
+++ engine/api/views.py
@@ -0,0 +1,6 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return JsonResponse("Hello, world. You're at the polls index.")
