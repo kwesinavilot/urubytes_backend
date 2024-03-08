c57fb9aec5754e86e1bee61de8d833c16ce6fe93
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2024-03-08 19:12:40 +0000

    Added Waitlist & Contact migrations

--- engine/api/admin.py
+++ engine/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Waitlist, Contact
 
 # Register your models here.
+admin.site.register(Waitlist)
+admin.site.register(Contact)
--- engine/api/models.py
+++ engine/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Waitlist(models.Model):
+    email = models.CharField(max_length=60)
+    phoneNumber = models.CharField(max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
+    
+class Contact(models.Model):
+    email = models.CharField(max_length=60)
+    phoneNumber = models.CharField(max_length=15)
+    message = models.CharField(max_length=60)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
