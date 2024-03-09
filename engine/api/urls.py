c15665d809aa2104ef2d9732093b75858fc9e298
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2024-03-09 10:06:13 +0000

    Added delete, update to Waitlist

--- engine/api/urls.py
+++ engine/api/urls.py
@@ -5,8 +5,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('waitlists/', views.getWaitlist, name='getwaitlists'),
-    path('waitlists/add', views.addToWaitlist, name='savewaitlist'),
+    path('waitlists/', views.getWaitlisters, name='getWaitlisters'),
+    path('waitlists/add', views.addToWaitlist, name='addWaitlister'),
+    path('waitlists/<int:pk>', views.getWaitlister, name='getWaitlister'),
+    path('waitlists/<int:pk>/update', views.updateWaitlister, name='updateWaitlister'),
+    path('waitlists/<int:pk>/delete', views.deleteWaitlister, name='deleteWaitlister'),
     # path('contact/', views.saveContact, name='savecontact'),
     # path('admin/', admin.site.urls),
 ]
--- engine/api/views.py
+++ engine/api/views.py
@@ -13,17 +13,63 @@ def index(request):
         "message": "We're alive! Let's change the world!!"
     })
 
-# create get endpoint for waitlist
+# create get all the waitlists
 @api_view(['GET'])
-def getWaitlist(request):
+def getWaitlisters(request):
     waitlist = Waitlist.objects.all()
     waitlistSerializer = WaitlistSerializer(waitlist, many=True)
-    return JsonResponse({"waitlist" : waitlistSerializer.data})
+    return Response(waitlistSerializer.data)
 
-# create post/save endpoint for waitlist
+# save someone to waitlist
 @api_view(['POST'])
 def addToWaitlist(request):
     waitlistSerializer = WaitlistSerializer(data=request.data)
     if waitlistSerializer.is_valid():
         waitlistSerializer.save()
         return Response(waitlistSerializer.data, status=status.HTTP_201_CREATED)
+    
+# get specific waitlister
+@api_view(['GET'])
+def getWaitlister(request, pk):
+    # try to get waitlist
+    try:
+        waitlister = Waitlist.objects.get(id=pk)
+    except Waitlist.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    waitlistSerializer = WaitlistSerializer(waitlister, many=False)
+    return Response(waitlistSerializer.data)
+
+# update waitlister
+@api_view(['PUT'])
+def updateWaitlister(request, pk):
+    # first, try to get waitlist
+    try:
+        waitlister = Waitlist.objects.get(id=pk)
+    except Waitlist.DoesNotExist:
+        # if not found, return 404
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    # 
+    waitlistSerializer = WaitlistSerializer(instance=waitlister, data=request.data)
+
+    # check if data is valid
+    if waitlistSerializer.is_valid():
+        # if valid, save and return the updated data
+        waitlistSerializer.save()
+        return Response(waitlistSerializer.data)
+    
+    # if not valid, return 400
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+# delete a waitlister
+@api_view(['DELETE'])
+def deleteWaitlister(request, pk):
+    try:
+        waitlister = Waitlist.objects.get(id=pk)
+    except Waitlist.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    waitlister.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
