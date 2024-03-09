74c390ae1025ad8c1db45bafc3b0d498fd3b5ff4
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2024-03-09 07:46:16 +0000

    Created post funct to add people to waitlist

--- engine/api/urls.py
+++ engine/api/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('waitlists/', views.getWaitlist, name='waitlist'),
+    path('waitlists/', views.getWaitlist, name='getwaitlists'),
+    path('waitlists/add', views.addToWaitlist, name='savewaitlist'),
+    # path('contact/', views.saveContact, name='savecontact'),
+    # path('admin/', admin.site.urls),
 ]
--- engine/api/views.py
+++ engine/api/views.py
@@ -2,16 +2,28 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from .models import Waitlist
 from .serializers import WaitlistSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+@api_view(['GET'])
 def index(request):
     return JsonResponse({
         "message": "We're alive! Let's change the world!!"
     })
 
 # create get endpoint for waitlist
+@api_view(['GET'])
 def getWaitlist(request):
     waitlist = Waitlist.objects.all()
-    print(waitlist)
     waitlistSerializer = WaitlistSerializer(waitlist, many=True)
-    return JsonResponse({"waitlist" : waitlistSerializer.data}, safe=False)
+    return JsonResponse({"waitlist" : waitlistSerializer.data})
+
+# create post/save endpoint for waitlist
+@api_view(['POST'])
+def addToWaitlist(request):
+    waitlistSerializer = WaitlistSerializer(data=request.data)
+    if waitlistSerializer.is_valid():
+        waitlistSerializer.save()
+        return Response(waitlistSerializer.data, status=status.HTTP_201_CREATED)
