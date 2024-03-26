651504a00f95954137e6e9c911f0d1eb877e2924
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2024-03-26 09:53:42 +0000

    Removed django_extensions

--- engine/settings.py
+++ engine/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # 3rd party
-    'django_extensions',
+    # 'django_extensions',
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
